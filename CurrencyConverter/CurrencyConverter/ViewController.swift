54dc9a1a5ea0d6fb6d558136398e11f736e168ba
fritzgt <tuanis@live.com>
2020-01-08 18:43:29 +0200

    completed MVP

--- CurrencyConverter/CurrencyConverter/ViewController.swift
+++ CurrencyConverter/CurrencyConverter/ViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+//Create a currency type enum
 enum Currency {
     case cad
     case mxn
@@ -24,21 +25,55 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var cadButton: UIButton!
     @IBOutlet weak var mxnButton: UIButton!
-
+    
+    //Create a property named currency of type Currency
+    var currency: Currency = Currency.cad
+    
+    
     // MARK: - Actions
     
     @IBAction func convertButtonTapped(_ sender: UIButton) {
-        
+        //early exit and unwrap text field
+        guard let value = fromCurrencyTextField.text, let doubleNum = Double(value) else { return }
+        //conver to double
+        let converted = convert(doubleNum)
+        //conver to text with two decimals
+        toCurrencyTextField.text = String(format: "%.2f", converted)
     }
     
+    
     @IBAction func cadButtonTapped(_ sender: UIButton) {
+        //Toggle this button's isSelected state
+        sender.isSelected = !sender.isSelected
+        //Toggle the mxn button's isSelected state
+        mxnButton.isSelected = !mxnButton.isSelected
+        //set the currency property to .cad
+        currency = Currency.cad
+        //Display the currency
+        toCurrencyLabel.text = "Currency (CAD)"
         
     }
     
     @IBAction func mxnButtonTapped(_ sender: UIButton) {
+        //Toggle this button's isSelected state
+        sender.isSelected = !sender.isSelected
+        //Toggle the cad button's isSelected state
+        cadButton.isSelected = !cadButton.isSelected
+        //set the currency property to .mxn
+        currency = Currency.mxn
+        //Display the currency
+        toCurrencyLabel.text = "Currency (MXN)"
         
+ 
     }
     
     // MARK: - Helper Methods
+    func convert(_ dollars: Double) -> Double {
+        if currency == Currency.cad {
+            return  dollars * 1.33
+        }else{
+             return  dollars * 19.70
+        }
+    }
 }
 
